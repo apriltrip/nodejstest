69dd0d45c6b3ea5f3e3b2ce4e3e4d60522094e09
OpenShift guest <apriltripstory@gmail.com>
2013-01-20 02:37:49 -0500

    cookie test

---
+++ cookie.test.js
@@ -0,0 +1,20 @@
+var http = require("http");
+
+http.createServer(function(req, res) 
+{
+    var date = new Date();
+    date.setDate(date.getDate() + 7);
+    
+    
+    res.writeHead(200, 
+                  {'Content-Type': 'text/html',
+                   'Set-Cookie': 
+                   ['breakfast = toast; Expires = ' + date.toUTCString(), 
+                    'dinner = chicken']
+                  });
+                  
+    res.end('<h1>' + req.headers.cookie + '</h1>');
+}).listen(process.env.PORT, function()
+{
+    console.log('Server Running');
+});

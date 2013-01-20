5cc5199a0f0963c745322722f745b03caa457816
OpenShift guest <apriltripstory@gmail.com>
2013-01-20 05:05:58 -0500

    update cookie test

--- cookie.test.js
+++ cookie.test.js
@@ -2,18 +2,39 @@ var http = require("http");
 
 http.createServer(function(req, res) 
 {
-    var date = new Date();
-    date.setDate(date.getDate() + 7);
+    // get cookie
+    var cookie = req.headers.cookie;
     
+    cookie = cookie.split(';').map(function(element)
+    {
+        var elementData = element.split('=');
+        
+        return {key: elementData[0],
+                value: elementData[1]};
+    });
     
+    // set cookie
     res.writeHead(200, 
                   {'Content-Type': 'text/html',
                    'Set-Cookie': 
-                   ['breakfast = toast; Expires = ' + date.toUTCString(), 
-                    'dinner = chicken']
+                   ['name = apriltrip', 
+                    'region = seoul']
                   });
                   
-    res.end('<h1>' + req.headers.cookie + '</h1>');
+    res.end('<h1>' + JSON.stringify(cookie) + '</h1>');
+
+//    var date = new Date();
+//    date.setDate(date.getDate() + 7);
+//    
+//    
+//    res.writeHead(200, 
+//                  {'Content-Type': 'text/html',
+//                   'Set-Cookie': 
+//                   ['breakfast = toast; Expires = ' + date.toUTCString(), 
+//                    'dinner = chicken']
+//                  });
+//                  
+//    res.end('<h1>' + req.headers.cookie + '</h1>');
 }).listen(process.env.PORT, function()
 {
     console.log('Server Running');

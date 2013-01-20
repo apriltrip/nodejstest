2fa9b5090d64034fb3792e91fbe0ee709f61dadf
OpenShift guest <apriltripstory@gmail.com>
2013-01-20 06:01:16 -0500

    update ejs test source

--- ejs_test/ejs.basic.js
+++ ejs_test/ejs.basic.js
@@ -7,7 +7,9 @@ http.createServer(function(req, res)
 	fs.readFile('ejs_test/EJSPage.ejs', 'utf8', function(error, data)
 	{
 		res.writeHead(200, {'Content-Type': 'text/html'});
-            	res.end(ejs.render(data));
+        res.end(ejs.render(data,
+        {name:'apriltrip',
+         description:'Hello EJS With Node.js'}));
 	});
 }).listen(process.env.PORT, function()
 {

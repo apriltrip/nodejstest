3faac443580b781a9c49be9061ffaf72e63d175e
apriltrip <apriltripstory@gmail.com>
2013-01-27 00:33:48 +0900

    upload socket.io module & sample source

---
+++ socket.io.chat/app.js
@@ -0,0 +1,28 @@
+var http = require('http');
+var fs = require('fs');
+var socketio = require('socket.io');
+
+var server = http.createServer(function(req, res)
+{
+	fs.readFile('socket.io.chat/HTMLPage.htm', function(error, data)
+	{
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.end(data);
+	});
+
+}).listen(52273, function()
+{
+	console.log('server running....');
+});
+
+var io = socketio.listen(server);
+
+io.sockets.on('connection', function(socket)
+{
+	socket.on('message', function(data)
+	{
+		console.log('receive message: ' + data);
+
+		io.sockets.emit('message', data);
+	});
+});

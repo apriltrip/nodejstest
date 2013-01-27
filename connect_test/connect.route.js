ba07a59b1069c931db8a0733a7251765463e5115
OpenShift guest <apriltripstory@gmail.com>
2013-01-27 03:13:26 -0500

    connect routing test

--- connect_test/connect.route.js
+++ connect_test/connect.route.js
@@ -1,20 +1,64 @@
 var connect = require("connect");
-var server = connect.createServer();
+//var server = connect.createServer();
+var server  = connect();
 
-server.use('/Home/Index', function(request, response, next)
+server.use(function(req, res)
 {
-        response.writeHead(200, {'Content-Type': 'text/html'});
-        response.write('<h1>Index Page</h1>');
-        response.end();
+    if (req.method == 'GET')
+    {
+        if (req.originalUrl == '/')
+        {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write('<h1>Home Page</h1>');
+            res.end();
+        }
+        else if (req.originalUrl == '/Test')
+        {
+            //res.end('Test from Connect!\n');
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write('<h1>Test Page</h1>');
+            res.end();
+        }
+        else if (req.originalUrl == '/About')
+        {
+            //res.end('About from Connect!\n');
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write('<h1>About Page</h1>');
+            res.end();
+        }
+    }
+    
 });
 
+//server.use('/', function(request, response, next)
+//{
+//    console.log(request.originalUrl);
+//    
+//    response.writeHead(200, {'Content-Type': 'text/html'});
+//    response.write('<h1>Home Page</h1>');
+//    response.end();
+//});
 
-server.use('/Home/About', function(request, response, next)
-{
-        response.writeHead(200, {'Content-Type': 'text/html'});
-        response.write('<h1>About Page</h1>');
-        response.end();
-});
+//server.use('/Home/Index', function(request, response, next)
+//server.use('/Index', function(request, response, next)
+//{
+//    console.log(request.originalUrl);
+//    
+//    response.writeHead(200, {'Content-Type': 'text/html'});
+//    response.write('<h1>Index Page</h1>');
+//    response.end();
+//});
+
+
+//server.use('/Home/About', function(request, response, next)
+//server.use('/About', function(request, response, next)
+//{
+//    console.log(request.originalUrl);
+//    
+//    response.writeHead(200, {'Content-Type': 'text/html'});
+//    response.write('<h1>About Page</h1>');
+//    response.end();
+//});
 
     
 server.listen(process.env.PORT, function()
